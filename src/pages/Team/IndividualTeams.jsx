41fded535b50fd7c02b73337209b2f4b90590298
Ryan Cho <ryancho7@uw.edu>
2025-02-20 02:52:14 -0800

    slight margin fixes

--- src/pages/Team/IndividualTeams.jsx
+++ src/pages/Team/IndividualTeams.jsx
@@ -3,7 +3,7 @@ import TeamCard from "./TeamCard";
 
 export default function IndividualTeams({ team, members }) {
     return (
-        <div className="flex flex-wrap items-start content-start gap-[40px] w-[1520px] min-h-[666px] flex-none order-0 self-stretch flex-grow-0">
+        <div className="flex flex-wrap items-start content-start gap-[40px] w-[1520px] flex-none order-0 self-stretch flex-grow-0">
             <div className="flex flex-row flex-wrap items-start content-start p-0 gap-[40px] w-[1520px] min-h-[576px]">
                 {members.length > 0 ? (
                     members.map((member, memberIndex) => (
