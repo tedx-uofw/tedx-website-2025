edcee43d2a17b5397a9d7f14757ef20f4d7a3ec0
ndaeila <ndaeila@uw.edu>
2025-02-23 18:59:59 -0800

    Schedule Refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import Schedule from '../components/schedule';
 import HeroSection from '../components/HomePage/HeroSection';
 import AboutSection from '../components/HomePage/AboutSection';
 import VideoSection from '../components/HomePage/VideoSection';
 import VenueAndAccessibilitySection from '../components/HomePage/VenueAndAccessibilitySection';
 import SponsorsSection from '../components/HomePage/SponsorsSection';
+import ScheduleSection from '../components/HomePage/ScheduleSection';
 
 const Home = () => {
   return (
@@ -12,7 +12,7 @@ const Home = () => {
       <AboutSection />
       <VideoSection />
       <div className="relative z-10 mt-[50px]">
-        <Schedule />
+        <ScheduleSection />
       </div>
       <VenueAndAccessibilitySection />
       <SponsorsSection />
