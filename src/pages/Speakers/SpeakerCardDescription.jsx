c4481c93dfb528495560f2b7c8740f41c9375eaf
ndaeila <ndaeila@uw.edu>
2025-02-26 01:56:24 -0800

    Another description formatting change

--- src/pages/Speakers/SpeakerCardDescription.jsx
+++ src/pages/Speakers/SpeakerCardDescription.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SpeakerCardDescription({description}) {
   return (
-    <div className="w-full lg:w-[740px] text-[#E4E0E4] font-abc-favorit-variable-edu text-xl sm:text-2xl md:text-[26px] lg:text-[28px] leading-[3em] tracking-[-0.01em]">
+    <div className="w-full lg:w-[740px] text-[#E4E0E4] font-abc-favorit-variable-edu text-xl sm:text-2xl md:text-[26px] lg:text-[28px] leading-[1.5em] sm:leading-[1.5em] tracking-[-0.01em]">
       {description}
     </div>
   )
