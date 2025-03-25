b9e67042fec4c459666ba7b2182448c8d61db3e6
ndaeila <ndaeila@uw.edu>
2025-03-25 00:23:59 -0400

    new file:   src/components/common/Icons.jsx

--- src/components/Footer/LegalIcons.jsx
+++ src/components/Footer/LegalIcons.jsx
@@ -1,26 +1,28 @@
-import { SiInstagram } from "react-icons/si";
-import { FaFacebookF } from "react-icons/fa";
-import { FaTiktok } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
-import { MdOutlineMail } from "react-icons/md";
+import { 
+  InstagramIcon, 
+  FacebookIcon, 
+  TiktokIcon, 
+  LinkedinIcon, 
+  EmailIcon 
+} from '../common/Icons';
 
 export default function LegalIcons() {
   return (
     <div className='flex flex-row items-center p-0 gap-[24px] w-[256px] h-[32px]'>
         <a href='' className='flex flex-col justify-center items-center p-0 gap-0 w-[32px] h-[32px] text-white'>
-            <SiInstagram className="h-[24px] w-[24px]"/>
+            <InstagramIcon className="h-[24px] w-[24px]"/>
         </a>
         <a href='' className='flex flex-col justify-center items-center p-0 gap-0 w-[32px] h-[32px] text-white'>
-            <FaFacebookF className="h-[24px] w-[24px]"/>
+            <FacebookIcon className="h-[24px] w-[24px]"/>
         </a>
         <a href='' className='flex flex-col justify-center items-center p-0 gap-0 w-[32px] h-[32px] text-white'>
-            <FaTiktok className="h-[24px] w-[24px]"/>
+            <TiktokIcon className="h-[24px] w-[24px]"/>
         </a>
         <a href='' className='flex flex-col justify-center items-center p-0 gap-0 w-[32px] h-[32px] text-white'>
-            <FaLinkedinIn className="h-[24px] w-[24px]"/>
+            <LinkedinIcon className="h-[24px] w-[24px]"/>
         </a>
         <a href='' className='flex flex-col justify-center items-center p-0 gap-0 w-[32px] h-[32px] text-white'>
-            <MdOutlineMail className="h-[24px] w-[24px]"/>
+            <EmailIcon className="h-[24px] w-[24px]"/>
         </a>
         
     </div>
